140a9a7033cec2e7fa5fce47f608433a67565469
pinnnnnzyyyyy <akunmlprib7900@gmail.com>
2025-12-31 18:14:47 +0700

    Create i18n.js

---
+++ js/i18n.js
@@ -0,0 +1,15 @@
+const langSelect = document.getElementById('langSelect');
+
+langSelect.addEventListener('change', () => {
+  loadLang(langSelect.value);
+});
+
+function loadLang(lang) {
+  fetch(`lang/${lang}.json`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById('appTitle').innerText = data.title;
+    });
+}
+
+loadLang('en');
